494b6c25fb43c7d67d2ac0a60c6afbd0e5e33bee
Alex Ninneman <alex.ninneman@couchbase.com>
2020-10-28 20:56:35 -0500

    starts building out collections interface

--- collections.go
+++ collections.go
@@ -2,6 +2,7 @@ package discogs
 
 import (
 	"context"
+	"strconv"
 	"strings"
 
 	"github.com/gomodule/oauth1/oauth"
@@ -10,6 +11,7 @@ import (
 
 type CollectionService interface {
 	GetFolders(ctx context.Context, username string, options ...Option) (*CollectionResponse, error)
+	GetFolder(ctx context.Context, args GetFolderArgs, options ...Option) (*Folder, error)
 }
 
 type collectionService struct {
@@ -19,7 +21,9 @@ type collectionService struct {
 }
 
 const (
-	collectionsURI = "/users/{username}/collection/folders"
+	foldersURI        = "/users/{username}/collection/folders"
+	folderURI         = "/users/{username}/collection/folders/{id}"
+	folderReleasesURI = "/users/{username}/collection/folders/{id}/releases"
 )
 
 func newCollectionService(url string) CollectionService {
@@ -47,18 +51,18 @@ func (c *collectionService) GetFolders(ctx context.Context, username string, opt
 		opts(c)
 	}
 
-	route := c.url + strings.Replace(collectionsURI, "{username}", username, 1)
+	path := strings.Replace(foldersURI, "{username}", username, 1)
 
 	span.AddAttributes(
 		trace.StringAttribute("username", username),
-		trace.StringAttribute("route", route),
+		trace.StringAttribute("path", path),
 	)
 
 	var collection CollectionResponse
 
 	if err := requestWithCreds(
 		ctx,
-		route,
+		c.url+path,
 		c.oauthClient,
 		c.creds,
 		nil,
@@ -73,4 +77,94 @@ func (c *collectionService) GetFolders(ctx context.Context, username string, opt
 	}
 
 	return &collection, nil
+}
+
+type GetFolderArgs struct {
+	ID       int
+	Username string
+}
+
+func (g GetFolderArgs) TraceAttributes() []trace.Attribute {
+	return []trace.Attribute{
+		trace.StringAttribute("username", g.Username),
+		trace.Int64Attribute("id", int64(g.ID)),
+	}
+}
+
+// GetFolder retrieves metadata associated with a particular folder.
+// /users/{username}/collection/folders/{id}
+func (c *collectionService) GetFolder(ctx context.Context, args GetFolderArgs, options ...Option) (*Folder, error) {
+	ctx, span := trace.StartSpan(ctx, "ninnemana.discogs.GetFolder")
+	defer span.End()
+
+	for _, opts := range options {
+		opts(c)
+	}
+
+	path := strings.Replace(folderURI, "{username}", args.Username, 1)
+	path = strings.Replace(path, "{id}", strconv.Itoa(args.ID), 1)
+
+	span.AddAttributes(args.TraceAttributes()...)
+	span.AddAttributes(trace.StringAttribute("path", path))
+
+	var folder Folder
+
+	if err := requestWithCreds(
+		ctx,
+		c.url+path,
+		c.oauthClient,
+		c.creds,
+		nil,
+		&folder,
+	); err != nil {
+		span.SetStatus(trace.Status{
+			Code: trace.StatusCodeInternal,
+		})
+		span.AddAttributes(trace.StringAttribute("err", err.Error()))
+
+		return nil, err
+	}
+
+	return &folder, nil
+}
+
+type FolderReleasesResponse struct {
+	Releases []Release `json:"releases"`
+}
+
+// GetFolder retrieves metadata associated with a particular folder.
+// /users/{username}/collection/folders/{id}/releases
+func (c *collectionService) GetFolderReleases(ctx context.Context, args GetFolderArgs, options ...Option) (*FolderReleasesResponse, error) {
+	ctx, span := trace.StartSpan(ctx, "ninnemana.discogs.GetFolderReleases")
+	defer span.End()
+
+	for _, opts := range options {
+		opts(c)
+	}
+
+	path := strings.Replace(folderReleasesURI, "{username}", args.Username, 1)
+	path = strings.Replace(path, "{id}", strconv.Itoa(args.ID), 1)
+
+	span.AddAttributes(args.TraceAttributes()...)
+	span.AddAttributes(trace.StringAttribute("path", path))
+
+	var releases FolderReleasesResponse
+
+	if err := requestWithCreds(
+		ctx,
+		c.url+path,
+		c.oauthClient,
+		c.creds,
+		nil,
+		&releases,
+	); err != nil {
+		span.SetStatus(trace.Status{
+			Code: trace.StatusCodeInternal,
+		})
+		span.AddAttributes(trace.StringAttribute("err", err.Error()))
+
+		return nil, err
+	}
+
+	return &releases, nil
 }
